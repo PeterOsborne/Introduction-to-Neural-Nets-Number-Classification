572462c2a08fda409db6b790d158d2262a667ade
Peter Osborne <z5480332@ad.unsw.edu.au>
2024-01-28 00:08:04 +1100

    started even classifier

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import numpy as np
+import random as rand
+import csv
+
+with open('file.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for i in range (100):
+        num = rand.randint(0,100)
+        even = 0 if (num % 2 == 0) else 1
+        writer.writerow([num, even])
+        i+=1
